35a6d1fc11ee83c226d3c12c4aab68c7a2d86ddf
fatthinh <pthinh.lama@gmail.com>
2024-10-04 13:53:30 +0700

    update

--- backend/notification-service/src/main/java/com/lpthinh/notificationservice/websocket/WebSocketController.java
+++ backend/notification-service/src/main/java/com/lpthinh/notificationservice/websocket/WebSocketController.java
@@ -1,9 +1,7 @@
 package com.lpthinh.notificationservice.websocket;
 
-import com.lpthinh.notificationservice.kafka.payment.PaymentNotification;
 import com.lpthinh.notificationservice.notification.Notification;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.handler.annotation.Payload;
